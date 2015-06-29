eab788bff1436b381cc1e3cf7f05f2d776dbd046
AbstractedNerve <abstracted.nerve@gmail.com>
2015-06-28 20:57:07 -0400

    strange

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -4,6 +4,7 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#include "main.h"
 #include "wallet.h"
 #include "walletdb.h"
 #include "crypter.h"
