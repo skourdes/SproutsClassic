9c9e1dd8820b62c85eafb1a9f229fc1d41c29b74
AbstractedNerve <abstracted.nerve@gmail.com>
2015-06-29 13:13:41 -0400

    diff change
    40 is crazy haha

--- src/main.cpp
+++ src/main.cpp
@@ -33,7 +33,7 @@ map<uint256, CBlockIndex*> mapBlockIndex;
 set<pair<COutPoint, unsigned int> > setStakeSeen;
 uint256 hashGenesisBlock = hashGenesisBlockOfficial;
 static CBigNum bnProofOfWorkLimit(~uint256(0) >> 20);
-static CBigNum bnInitialHashTarget(~uint256(0) >> 40);
+static CBigNum bnInitialHashTarget(~uint256(0) >> 20);
 unsigned int nStakeMinAge = STAKE_MIN_AGE;
 int nCoinbaseMaturity = COINBASE_MATURITY_PPC;
 CBlockIndex* pindexGenesisBlock = NULL;
