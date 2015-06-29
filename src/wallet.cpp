87a81b8ea6d3a2132522c4f64c936eca11e0043a
AbstractedNerve <abstracted.nerve@gmail.com>
2015-06-28 20:46:59 -0400

    test

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -1235,7 +1235,8 @@ bool CWallet::CreateCoinStake(const CKeyStore& keystore, unsigned int nBits, int
     // The following split & combine thresholds are important to security
     // Should not be adjusted if you don't understand the consequences
     static unsigned int nStakeSplitAge = (60 * 60 * 24 * 90);
-    int64 nCombineThreshold = GetProofOfWorkReward(GetLastBlockIndex(pindexBest, false)->nBits, GetLastBlockIndex(pindexBest, false)->nHeight) / 3;
+    const CBlockIndex* pIndex0 = GetLastBlockIndex(pindexBest, false);
+    int64 nCombineThreshold = GetProofOfWorkReward(pIndex0->nBits, pIndex0->nHeight) / 3;
 
     CBigNum bnTargetPerCoinDay;
     bnTargetPerCoinDay.SetCompact(nBits);
