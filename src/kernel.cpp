c568a99c6f9922fc77023d8134d2500ab472a9cd
AbstractedNerve <abstracted.nerve@gmail.com>
2015-06-28 20:28:09 -0400

    update checkpoints

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -30,9 +30,6 @@ unsigned int nModifierInterval = MODIFIER_INTERVAL;
 static std::map<int, unsigned int> mapStakeModifierCheckpoints =
     boost::assign::map_list_of
     ( 0, 0x0e00670bu )
-    ( 19080, 0xad4e4d29u )
-    ( 30583, 0xdc7bf136u )
-    ( 99999, 0xf555cfd2u )
     ;
 
 // Whether the given coinstake is subject to new v0.3 protocol
